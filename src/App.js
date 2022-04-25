52c22c7012c8b174b6c7e72deab6040d1018bc9f
yuriMartins <ysm281@gmail.com>
2022-04-25 00:13:10 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+
+class App extends Component{
+  render(){
+    return (
+      <div className= " app" >
+          <h1>Bora</h1> 
+      </div>
+    );
+
+  }
+}
+
+
+export default App;
