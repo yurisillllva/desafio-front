6b56556af109a2ac957ba572958d31984030f98f
yuriMartins <ysm281@gmail.com>
2022-04-27 03:11:24 -0300

    Add das informações e estilização da pg detalhes produtos

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import {useEffect, useState} from 'react';
 
 import './product-info.css';
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import api from "../../services/api";
 
 export default function Product(){
     const { id  } = useParams();
+    const history = useHistory();
+
     const [produto, setProduto] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -14,6 +16,13 @@ export default function Product(){
 
         async function loadProducts(){
             const response = await api.get(`http://localhost:3000/products/${id}`)
+
+            if(response.data.length === 0){
+                //tentando acessar produto inexistente
+                history.replace('/');
+                return;
+            }
+
             //console.log(response.data);
             setProduto(response.data);
             setLoading(false);
@@ -21,7 +30,11 @@ export default function Product(){
 
         loadProducts();
 
-    }, [id]);
+        return () => {
+            console.log('Componente desmontado')
+        }
+
+    }, [history, id]);
 
     if(loading){
         return(
@@ -33,7 +46,17 @@ export default function Product(){
 
    return( 
     <div className="produto-info">
-        <h1>PAGINA DETALHES - {id} </h1>
+        <h1> {produto.title} </h1>
+        <img src={produto.image} alt={produto.title} />
+        <h3>Descrição</h3>
+        {produto.description}
+        <h2>Preço</h2>
+        {produto.price}
+
+        <div className="botao">
+            <button onClick={()=>{}}>Salvar</button>
+        </div>
+
     </div>
    ) 
 }
